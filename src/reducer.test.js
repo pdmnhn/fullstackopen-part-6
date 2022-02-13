fd890147d6929691cf9f0b9710f6747017c38712
Padmanabhan K <dev.pdmnhn@gmail.com>
2022-02-13 19:42:55 +0530

    Exercises 6.1 - 6.2 complete

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -31,4 +31,45 @@ describe("unicafe reducer", () => {
       bad: 0,
     });
   });
+
+  test("ok is incremented", () => {
+    const action = {
+      type: "OK",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    });
+  });
+
+  test("bad is incremented", () => {
+    const action = {
+      type: "BAD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    });
+  });
+
+  test("reset to zero", () => {
+    const action = {
+      type: "ZERO",
+    };
+    const state = { good: 10, ok: 23, bad: 12 };
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual(initialState);
+  });
 });
